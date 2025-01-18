09bd1d719eb41a0d627d7152df7d705d28943d61
jeffkao1124 <69136565+jeffkao1124@users.noreply.github.com>
2025-01-18 16:20:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,66 +30,6 @@ line_bot_api = LineBotApi('JHhhyAGkeAv1E5c3ylrrPoC1xj5nVbjyxpvFKsenCqcZ43aE1ozhe
 # Channel Secret
 handler = WebhookHandler('4208ae25a46b2c44a6a1823268d22232')
 
-@app.route("/")
-def home():
-    return 'home OK'
-
-#爬蟲取得匯率
-def get_TodayRate(mode):
-    numb= []
-    cate=[]
-    data=[]
-    url_1= "https://rate.bot.com.tw/xrt?Lang=zh-TW"
-    resp_1 = requests.get(url_1)
-    ms =BeautifulSoup(resp_1.text,"html.parser")
-
-    t1=ms.find_all("td","rate-content-cash text-right print_hide")
-    for child in t1:
-        numb.append(child.text.strip())
-
-    buy=numb[0:37:2]
-    sell=numb[1:38:2]
-
-    t2=ms.find_all("div","hidden-phone print_show")
-    for child in t2:
-        cate.append(child.text.strip())
-    for i in range(19):
-        data.append([cate[i] +'買入：'+buy[i]+ '賣出：'+sell[i]])
-
-    if mode==1:
-        USD = data[0][0]
-        regex = re.compile(r'賣出：(\d+.*\d*)')
-        match = regex.search(USD)
-        return eval(match.group(1))
-    elif mode==2:
-        JPY = data[7][0]
-        regex = re.compile(r'賣出：(\d+.*\d*)')
-        match = regex.search(JPY)
-        return eval(match.group(1))
-    elif mode==3:
-        EUR = data[14][0]
-        regex = re.compile(r'賣出：(\d+.*\d*)')
-        match = regex.search(EUR)
-        return eval(match.group(1))
-    else:
-        return 1
-
-def get_Boardgame():
-    boardgame= []
-    url_1= "https://boardgamegeek.com/browse/boardgame"
-    resp_1 = requests.get(url_1)
-    ms = BeautifulSoup(resp_1.text,"html.parser")
-
-    for i in range(50):
-        t1=ms.find_all("div",id="results_objectname"+str(i))
-        for bgg in t1:
-            boardgame.append(bgg.text.strip())
-
-    return boardgame
-
-
-
-
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -1165,12 +1105,11 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage (output_text) )
         
 
+
+import os
 if __name__ == "__main__":
-    app.run()
-# import os
-# if __name__ == "__main__":
-#     port = int(os.environ.get('PORT', 5000))
-#     app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
