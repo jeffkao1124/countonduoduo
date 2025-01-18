d3e2250cb5ebeabd39056c613029e2fb26ad753a
jeffkao1124 <69136565+jeffkao1124@users.noreply.github.com>
2025-01-18 17:15:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 
-#import requests
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -19,6 +20,58 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('JHhhyAGkeAv1E5c3ylrrPoC1xj5nVbjyxpvFKsenCqcZ43aE1ozhe61IzeVWvHuG8eHehfneXTeNyNVeWig3ThlWoECTzs67Ns6cs4GAINdT4my4BC6xgY88Wn7jAz/cUDpaXEP2FI3y7w7OHYpPVgdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('4208ae25a46b2c44a6a1823268d22232')
+
+def get_TodayRate(mode):
+    numb= []
+    cate=[]
+    data=[]
+    url_1= "https://rate.bot.com.tw/xrt?Lang=zh-TW"
+    resp_1 = requests.get(url_1)
+    ms =BeautifulSoup(resp_1.text,"html.parser")
+
+    t1=ms.find_all("td","rate-content-cash text-right print_hide")
+    for child in t1:
+        numb.append(child.text.strip())
+
+    buy=numb[0:37:2]
+    sell=numb[1:38:2]
+
+    t2=ms.find_all("div","hidden-phone print_show")
+    for child in t2:
+        cate.append(child.text.strip())
+    for i in range(19):
+        data.append([cate[i] +'買入：'+buy[i]+ '賣出：'+sell[i]])
+
+    if mode==1:
+        USD = data[0][0]
+        regex = re.compile(r'賣出：(\d+.*\d*)')
+        match = regex.search(USD)
+        return eval(match.group(1))
+    elif mode==2:
+        JPY = data[7][0]
+        regex = re.compile(r'賣出：(\d+.*\d*)')
+        match = regex.search(JPY)
+        return eval(match.group(1))
+    elif mode==3:
+        EUR = data[14][0]
+        regex = re.compile(r'賣出：(\d+.*\d*)')
+        match = regex.search(EUR)
+        return eval(match.group(1))
+    else:
+        return 1
+
+def get_Boardgame():
+    boardgame= []
+    url_1= "https://boardgamegeek.com/browse/boardgame"
+    resp_1 = requests.get(url_1)
+    ms = BeautifulSoup(resp_1.text,"html.parser")
+
+    for i in range(50):
+        t1=ms.find_all("div",id="results_objectname"+str(i))
+        for bgg in t1:
+            boardgame.append(bgg.text.strip())
+
+    return boardgame
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -35,18 +88,18 @@ def callback():
         abort(400)
     return 'OK'
 
-# 處理訊息
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     message = TextSendMessage(text=event.message.text)
-#     line_bot_api.reply_message(event.reply_token, message)
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     input_text = event.message.text
+    if input_text=='@桌遊':
+        output_text = str(get_Boardgame())
 
-    if input_text == '啾啾啾':
+    elif input_text=='@電影':
+        output_text = str(get_movie())
+        
+    elif input_text == '啾啾啾':
         output_text = '啾啾啾'
 
     elif input_text == '逛夜市' or input_text == '烤小鳥' or input_text == '@吳柏震 ' or input_text == '@林瑋晟 ' or input_text == '@王奕凱 ' or input_text == '@高子承 ' or input_text == '@廖奕翔 ':
