3a7c3c12a0b556d4e4658f868d9ed4095de4eec9
jeffkao1124 <69136565+jeffkao1124@users.noreply.github.com>
2025-01-18 16:32:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,60 @@ from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 
+import requests
 
+#爬蟲取得匯率
+def get_TodayRate(mode):
+    numb= []
+    cate=[]
+    data=[]
+    url_1= "https://rate.bot.com.tw/xrt?Lang=zh-TW"
+    resp_1 = requests.get(url_1)
+    ms =BeautifulSoup(resp_1.text,"html.parser")
+
+    t1=ms.find_all("td","rate-content-cash text-right print_hide")
+    for child in t1:
+        numb.append(child.text.strip())
+
+    buy=numb[0:37:2]
+    sell=numb[1:38:2]
+
+    t2=ms.find_all("div","hidden-phone print_show")
+    for child in t2:
+        cate.append(child.text.strip())
+    for i in range(19):
+        data.append([cate[i] +'買入：'+buy[i]+ '賣出：'+sell[i]])
+
+    if mode==1:
+        USD = data[0][0]
+        regex = re.compile(r'賣出：(\d+.*\d*)')
+        match = regex.search(USD)
+        return eval(match.group(1))
+    elif mode==2:
+        JPY = data[7][0]
+        regex = re.compile(r'賣出：(\d+.*\d*)')
+        match = regex.search(JPY)
+        return eval(match.group(1))
+    elif mode==3:
+        EUR = data[14][0]
+        regex = re.compile(r'賣出：(\d+.*\d*)')
+        match = regex.search(EUR)
+        return eval(match.group(1))
+    else:
+        return 1
+
+def get_Boardgame():
+    boardgame= []
+    url_1= "https://boardgamegeek.com/browse/boardgame"
+    resp_1 = requests.get(url_1)
+    ms = BeautifulSoup(resp_1.text,"html.parser")
+
+    for i in range(50):
+        t1=ms.find_all("div",id="results_objectname"+str(i))
+        for bgg in t1:
+            boardgame.append(bgg.text.strip())
+
+    return boardgame
 app = Flask(__name__)
 
 # Channel Access Token
